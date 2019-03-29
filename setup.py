150463f5d27ddb0fd212c323a932b5282d53fb23
David Kane <d.r.kane@gmail.com>
2019-03-29 10:03:53 +0000

    update twisted

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="findthatcharity_import",
+    packages=["findthatcharity_import"],
+    version="0.0.1",
+    author="David Kane david@dkane.net",
+    include_package_data=True,
+    license="MIT",
+    description="Scrapers for findthatcharity",
+    install_requires=[
+        "Scrapy==1.5.1",
+        "validators==0.12.2",
+        "titlecase==0.12.0".
+        "tqdm==4.26.0",
+        "openpyxl==2.5.7",
+    ]
+)
