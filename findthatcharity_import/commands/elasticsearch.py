9966950e050490c2d0d15f67afdec747f928d4ec
David Kane <d.r.kane@gmail.com>
2019-06-17 10:03:31 +0100

    add reset option for elasticsearch index

--- findthatcharity_import/commands/elasticsearch.py
+++ findthatcharity_import/commands/elasticsearch.py
@@ -13,14 +13,18 @@ class Command(ScrapyCommand):
     def short_desc(self):
         return "Manage elasticsearch indices"
 
+    def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option("-r", "--reset", action="store_true",
+                          help="Reset the index before creating (WARNING: WILL DELETE DATA)")
+
+
     def run(self, args, opts):
 
-        print(args)
-
         if self.settings.get("ES_URL") is None:
+            logging.info("[elasticsearch] Can't connect - no connection information present")
+            logging.info("[elasticsearch] Use the ES_URL setting to connect to elasticsearch")
             return
-
-        # @TODO: add a method which sets all the current records from a spider to inactive (or deletes them)
 
         es = Elasticsearch(self.settings.get("ES_URL"))
         if not es.ping():
@@ -29,6 +33,12 @@ class Command(ScrapyCommand):
         
         for i in [Organisation, Source]:
             index_name = i.__name__.lower()
+
+            if opts.get('reset') and es.indices.exists(index=index_name):
+                confirm = input("This will delete index \"{}\" - are you sure? [y]es/[n]o".format(index_name))
+                if confirm[0:1].lower()=="y":
+                    es.indices.delete(index=index_name)
+
             if not es.indices.exists(index=index_name):
                 if hasattr(i, "es_mapping"):
                     es.indices.create(index_name, {
