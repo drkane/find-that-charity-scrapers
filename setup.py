f90a403cdfc987436dabac3bb2fb062f624b220b
David Kane <d.r.kane@gmail.com>
2019-07-04 21:49:51 +0100

    add additional dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="findthatcharity_import",
     packages=find_packages(),
-    version="0.0.1",
+    version="0.0.2",
     author="David Kane david@dkane.net",
     include_package_data=True,
     license="MIT",
@@ -14,5 +14,6 @@ setup(
         "titlecase==0.12.0",
         "tqdm==4.26.0",
         "openpyxl==2.5.7",
+        "SQLAlchemy==1.3.1",
     ]
 )
