a3ebcbda890fa1cc731a32ad7fdb4bc12a4475d6
David Kane <david@dkane.net>
2020-01-24 00:56:15 +0000

    change setup requirements

--- setup.py
+++ setup.py
@@ -9,18 +9,20 @@ setup(
     license="MIT",
     description="Scrapers for findthatcharity",
     install_requires=[
-        "alembic==1.3.2",
+        "alembic==1.3.3",
         "Scrapy==1.8.0",
-        "validators==0.12.6",
+        "validators==0.14.1",
         "titlecase==0.12.0",
         "tqdm==4.41.1",
         "openpyxl==3.0.3",
-        "SQLAlchemy==1.3.12",
+        "SQLAlchemy==1.3.13",
         "psycopg2==2.8.4",
+        "psycopg2-binary==2.8.4",
         "pyexcel-ezodf==0.3.4",
         "pyexcel-io==0.5.20",
         "pyexcel-ods3==0.5.3",
         "pymongo==3.10.1",
+        "bcp-reader==0.1.1",
     ],
     entry_points={
         'scrapy': [
