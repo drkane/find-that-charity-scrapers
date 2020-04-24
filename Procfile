2c91a738b689368e6e6c5a195093744e2b8acfb1
David Kane <david@dkane.net>
2020-04-24 18:13:42 +0100

    move database migration to procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: echo "Ready to scrape"
 cron: sleep infinity
+release: alembic upgrade head
--- crawl_all.sh
+++ crawl_all.sh
@@ -1,4 +1,3 @@
-alembic upgrade head
 for i in $(scrapy list); 
     do scrapy crawl "$i" -s DB_URI="$DB_URI"; 
 done; 
